2c86a96d34a974a9c83f4c5ae9126b1b9b389892
sharmapuneet20 <107070137+sharmapuneet20@users.noreply.github.com>
2023-06-17 21:16:57 +0530

    Add files via upload

---
+++ trendy_number.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+//public class trendy_number {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int num = 133;
+//        int q;
+//        num /=10;
+//        q = num %10 ;
+//        if (num>99 && q %3==0){
+//            System.out.println("trendy number");
+//
+//
+//        }
+//
+//
+//
+//
+//
+//
+//    }
+//}
